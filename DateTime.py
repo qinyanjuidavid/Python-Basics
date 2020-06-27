5ac5b3be85345ddb843d982fd6b4d13a96a8095c
qinyanjuidavid <davidkinyanjui052@gmail.com>
2020-06-27 19:23:28 +0300

    Just Floating on DateTime module but i ll learn more

---
+++ DateTime.py
@@ -0,0 +1,15 @@
+import time
+time=time.time()
+print(time)
+import time
+print(time.localtime())
+print(time.localtime(time.time()))
+import calendar
+print(calendar.month(2020,7))
+def Procedure():
+    time.sleep(1)
+    print("1 Sec Wasted for me to compile")
+if __name__=="__main__":
+    Procedure()
+import datetime
+#################################Need Resources to learn This################
