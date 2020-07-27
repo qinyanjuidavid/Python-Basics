80265d7155fefbb31509c0291e1930c3a850122b
qinyanjuidavid <davidkinyanjui052@gmail.com>
2020-07-27 22:45:13 +0300

    Initial file

--- CGIProgramming.py
+++ CGIProgramming.py
@@ -1,1 +1,20 @@
 # CGI stands for common gateway interface
+class LazyClass:
+    def __init__(self,width,length,height):
+        """:arg"""
+        self.width=width
+        self.length=length
+        self.height=height
+    def getMeasurements(self):
+        print (f"""
+Height:{self.height}
+Width: {self.width} 
+Height: {self.height}""")
+
+if __name__=="__main__":
+    day=LazyClass(10,20,30)
+    day.getMeasurements()
+class BusyClass:
+    def __init__(self):
+        """:arg"""
+        pass
